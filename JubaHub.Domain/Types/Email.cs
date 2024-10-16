d1f9014f8b08d03291b520067ccc844a50e57283
Lucca Giffoni <81778943+LuccaGiffoni@users.noreply.github.com>
2024-10-16 16:35:20 -0300

    feat: add email and phone number custom types

---
+++ JubaHub.Domain/Types/Email.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using JubaHub.Domain.Validators;
+
+namespace JubaHub.Domain.Types;
+
+public class Email
+{
+    public string Value { get; private set; }
+
+    public Email(string value)
+    {
+        Value = value;
+
+        var validator = new EmailValidator();
+        var result = validator.Validate(this);
+
+        if (!result.IsValid)
+            throw new ValidationException(result.Errors);
+    }
+
+    public override string ToString()
+        => Value;
+
+    public override bool Equals(object obj)
+        => obj is Email otherEmail && Value.Equals(otherEmail.Value, StringComparison.OrdinalIgnoreCase);
+
+    public override int GetHashCode()
+        => Value.GetHashCode();
+}
---
+++ JubaHub.Domain/Types/PhoneNumber.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using JubaHub.Domain.Validators;
+
+namespace JubaHub.Domain.Types;
+
+public class PhoneNumber
+{
+    public string Value { get; private set; }
+
+    public PhoneNumber(string value)
+    {
+        Value = value;
+
+        var validator = new PhoneNumberValidator();
+        var result = validator.Validate(this);
+
+        if (!result.IsValid)
+            throw new ValidationException(result.Errors);
+    }
+
+    public override string ToString()
+        => Value;
+
+    public override bool Equals(object obj)
+        => obj is PhoneNumber otherPhoneNumber && Value.Equals(otherPhoneNumber.Value);
+
+    public override int GetHashCode()
+        => Value.GetHashCode();
+}
